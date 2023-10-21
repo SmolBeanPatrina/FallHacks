8d2736b5af630b03375909c534f94f878e955e44
darksunlight <darksunlight210@gmail.com>
2023-10-21 15:08:45 -0700

    Goals empty state

---
+++ frontend/src/pages/Goals.tsx
@@ -0,0 +1,25 @@
+import dreamer from "../assets/undraw_dreamer.svg"
+
+function Goal() {
+  return (
+    <div>
+      <span>This is a goal</span>
+    </div>
+  )
+}
+
+function NoGoals() {
+  return (
+    <div className="flex flex-col justify-center h-full p-8 text-gray-700">
+      <img src={dreamer} alt="No goals yet" className="w-72" />
+      <h1 className="text-xl font-semibold mt-4">It's empty in here...</h1>
+      <p>We are ready to help you turn over a new leaf!</p>
+    </div>
+  )
+}
+
+function Goals() {
+  return <NoGoals />
+}
+
+export default Goals
