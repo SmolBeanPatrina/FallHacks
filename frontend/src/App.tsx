08320577837afb488e919ba0318e7f193b466aeb
darksunlight <darksunlight210@gmail.com>
2023-10-21 15:08:45 -0700

    Goals empty state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
+import { Routes, Route, Outlet } from "react-router-dom"
 // import imgsrc from '[./assets]/path'
 import "./App.css"
 import NavBar from "./components/NavBar"
-import { Routes, Route, Outlet } from "react-router-dom"
+import Goals from "./pages/Goals"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="goals" element={<h1 className="text-3xl">Goals!</h1>} />
+        <Route path="goals" element={<Goals />} />
         <Route path="explore" element={<h1 className="text-3xl">Explore!</h1>} />
         <Route path="profile" element={<h1 className="text-3xl">Profile!</h1>} />
         <Route path="*" element={<h1>Not found!</h1>} />
