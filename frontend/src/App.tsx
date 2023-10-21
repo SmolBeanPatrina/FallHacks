3eb5a1d664b29fdf9add7ce143d592f8e15e38c3
darksunlight <darksunlight210@gmail.com>
2023-10-21 16:03:35 -0700

    basic profile layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Outlet } from "react-router-dom"
 import "./App.css"
 import NavBar from "./components/NavBar"
 import Goals from "./pages/Goals"
+import Profile from "./pages/Profile"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +14,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route path="goals" element={<Goals />} />
         <Route path="explore" element={<h1 className="text-3xl">Explore!</h1>} />
-        <Route path="profile" element={<h1 className="text-3xl">Profile!</h1>} />
+        <Route path="profile" element={<Profile />} />
         <Route path="*" element={<h1>Not found!</h1>} />
       </Route>
     </Routes>
