0e9f1d67a2fb3f1e56bba56da59d8d010ce41ddf
Kelly Feng <kfa48@sfu.ca>
2023-10-21 17:52:35 -0700

    auth

--- Python/flaskr/auth.py
+++ Python/flaskr/auth.py
@@ -9,6 +9,17 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
