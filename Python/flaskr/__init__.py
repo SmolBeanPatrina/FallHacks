7e59378a0a0e0b446ffec965cffc7e3711e62169
darksunlight <darksunlight210@gmail.com>
2023-10-21 20:51:21 -0700

    cors

--- Python/flaskr/__init__.py
+++ Python/flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 
 def create_app(test_config=None):
@@ -10,6 +11,10 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
