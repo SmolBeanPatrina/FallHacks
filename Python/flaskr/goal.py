8e809d842f92b19c01eb3a1d126fb0ca79942b86
Kelly Feng <kfa48@sfu.ca>
2023-10-21 17:26:42 -0700

    Added more endpoints

--- Python/flaskr/goal.py
+++ Python/flaskr/goal.py
@@ -5,42 +5,49 @@ from flaskr.db import get_db
 
 bp = Blueprint('goal', __name__, url_prefix='/goal')
 
-@bp.route("/goals", methods = ["GET"])
+@bp.route("/", methods = ["GET"])
 def index():
-    """Show all the goals, most recent first."""
     db = get_db()
     goals = db.execute(
         "SELECT g.id, goalName, created, frequency, completed, numCompleted"
-        " FROM goal g"
-        " ORDER BY created DESC"
+        " FROM Goal g"
+        " ORDER BY created ASC"
     ).fetchall()
-    dbJSON = json.dumps(goals)
-    return dbJSON
+    goalsJSON = json.dumps(goals)
+    return goalsJSON
+
+@bp.route('/goal', methods=['GET'])
+def get_goal(id):
+    db = get_db()
+    goal = db.execute(
+        "SELECT g.id, goalName, created, frequency, completed, numCompleted"
+        " FROM goal g"
+        " WHERE g.id = ?",
+        (id,),
+    ).fetchone()
+    goalJSON = json.dumps(goal)
+    return goalJSON
+
+@bp.route('/delete', methods=['POST'])
+def delete(id):
+    get_goal(id)
+    db = get_db()
+    db.execute("DELETE FROM Goal WHERE id = ?", (id,))
+    db.commt()
+    return redirect(url_for("goal.index"))
+
+@bp.route('/achievements', methods=['GET'])
+def achievements():
+    db = get_db()
+    achievements = db.execute(
+        "SELECT a.id, completed, goal_id"
+        " FROM Achievement a"
+        " ORDER BY completed ASC"
+    ).fetchall()
+    achievementsJSON = json.dumps(achievements)
+    return achievementsJSON
 
 
-# @bp.route('/addGoal', methods=('GET'))
-# def getGoal(id):
-#     goal = (
-#         get_db()
-#         .execute(
-#             "SELECT"
-#         )
-#     )
 
-# @bp.route('/addGoal', methods=('POST'))
-# def addGoal():
-#     goalName = request.form['goalName']
-#     frequency = request.form['frequency']
 
-# @bp.route('/editGoal', methods=('GET', 'POST'))
-# def addGoal():
-#     goalName = request.form['goalName']
-#     frequency = request.form['frequency']
 
-# @bp.route('/<int:id>/delete', methods=('POST',))
-# def delete(id):
-#     get_post(id)
-#     db = get_db()
-#     db.execute('DELETE FROM post WHERE id = ?', (id,))
-#     db.commit()
-#     return redirect(url_for('blog.index'))
